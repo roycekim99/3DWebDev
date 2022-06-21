80da79c1eb0c796d0056baa5866a6c4eddfb0c5a
Royce-Work Computer <roycekim99@users.noreply.github.com>
2022-06-20 23:11:26 -0700

    rotating donut in space

--- 3D Web Development Test1/main.js
+++ 3D Web Development Test1/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // MARK: renderer setup
 const scene = new THREE.Scene();
@@ -24,20 +25,60 @@ renderer.render(scene, camera);
 
 // MARK: Geometry
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xff5347,
-  wireframe: true,
 });
 
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+// MARK: light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// MARK: Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(lightHelper, gridHelper);
+
+// MARK: OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// MARK: addStars
+function addStar() {
+  const geo = new THREE.SphereGeometry(0.25, 24, 24);
+  const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+  });
+
+  const star = new THREE.Mesh(geo, mat);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("stars.jpg");
+scene.background = spaceTexture;
+
+// MARK: Animation
 function animate() {
   requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
+  torus.rotation.y += 0.005;
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
